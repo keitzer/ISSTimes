c5fc3c9fe43bc0f294a7a22908276f8273c98bda
Alex Ogorek <ogoalex57@gmail.com>
2018-01-25 19:36:51 -0500

    pass over correctly passing

--- ISS/Data Types/PassOver.swift
+++ ISS/Data Types/PassOver.swift
@@ -8,7 +8,35 @@
 
 import Foundation
 
-struct PassOver {
+class PassOver: Equatable {
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    static func ==(lhs: PassOver, rhs: PassOver) -> Bool {
+        return lhs.duration == rhs.duration
+            && lhs.risetime == rhs.risetime
+    }
+    
     let duration: Int
     let risetime: Int
+    
+    init(duration: Int, risetime: Int) {
+        self.duration = duration
+        self.risetime = risetime
+    }
+    
+    init?(fromDict dictionary: [String: Any]) {
+        guard let duration = dictionary["duration"] as? Int,
+            let risetime = dictionary["risetime"] as? Int else {
+                return nil
+        }
+        
+        self.duration = duration
+        self.risetime = risetime
+    }
 }
--- ISSTests/Data Types/PassOverTests.swift
+++ ISSTests/Data Types/PassOverTests.swift
@@ -14,34 +14,74 @@ import Nimble
 class PassOverTests: QuickSpec {
     override func spec() {
         describe("PassOver") {
-            it("returns true when all are equal") {
-                let item1 = PassOver(duration: 10, risetime: 100)
-                let item2 = PassOver(duration: 10, risetime: 100)
+            describe("equality") {
+                it("returns true when all are equal") {
+                    let item1 = PassOver(duration: 10, risetime: 100)
+                    let item2 = PassOver(duration: 10, risetime: 100)
+                    
+                    expect(item1).to(equal(item2))
+                }
                 
-                expect(item1).to(equal(item2))
+                it("returns false when duration is different") {
+                    let item1 = PassOver(duration: 10, risetime: 100)
+                    let item2 = PassOver(duration: 20, risetime: 100)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+                
+                it("returns false when risetime is different") {
+                    let item1 = PassOver(duration: 10, risetime: 100)
+                    let item2 = PassOver(duration: 10, risetime: 200)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
             }
             
-            it("returns false when duration is different") {
-                let item1 = PassOver(duration: 10, risetime: 100)
-                let item2 = PassOver(duration: 20, risetime: 100)
+            describe("init from dictionary") {
+                it("can be loaded from dictionary") {
+                    let item = PassOver(fromDict: [
+                            "duration" : 50,
+                            "risetime" : 1000,
+                        ])
+                    
+                    expect(item).notTo(beNil())
+                    expect(item?.duration).to(equal(50))
+                    expect(item?.risetime).to(equal(1000))
+                }
                 
-                expect(item1).notTo(equal(item2))
-            }
-            
-            it("returns false when risetime is different") {
-                let item1 = PassOver(duration: 10, risetime: 100)
-                let item2 = PassOver(duration: 10, risetime: 200)
+                it("returns nil when duration is missing") {
+                    let item = PassOver(fromDict: [
+                            "risetime" : 1000
+                        ])
+                    
+                    expect(item).to(beNil())
+                }
                 
-                expect(item1).notTo(equal(item2))
-            }
-            
-            it("can be loaded from dictionary") {
-                let item = PassOver(fromDict: [
-                    "name" : "Value",
-                    "image" : "Image name",
-                    ])
+                it("returns nil when risetime is missing") {
+                    let item = PassOver(fromDict: [
+                            "duration" : 50
+                        ])
+                    
+                    expect(item).to(beNil())
+                }
                 
-                expect(item).notTo(beNil())
+                it("returns nil when duration cannot be cast to an int directly") {
+                    let item = PassOver(fromDict: [
+                            "duration" : "500",
+                            "risetime" : 1000,
+                        ])
+                    
+                    expect(item).to(beNil())
+                }
+                
+                it("returns nil when risetime cannot be cast to an int directly") {
+                    let item = PassOver(fromDict: [
+                            "duration" : 50,
+                            "risetime" : "1000",
+                        ])
+                    
+                    expect(item).to(beNil())
+                }
             }
         }
     }
