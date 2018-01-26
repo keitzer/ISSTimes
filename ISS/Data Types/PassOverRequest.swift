d0e317afb8973b90024dc5f23188e7fe17997306
Alex Ogorek <ogoalex57@gmail.com>
2018-01-25 19:41:27 -0500

    started pass over request tests

--- ISS/Data Types/PassOverRequest.swift
+++ ISS/Data Types/PassOverRequest.swift
@@ -8,10 +8,34 @@
 
 import Foundation
 
-struct PassOverRequest {
+class PassOverRequest: Equatable {
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    static func ==(lhs: PassOverRequest, rhs: PassOverRequest) -> Bool {
+        return lhs.altitude == rhs.altitude
+            && lhs.datetime == rhs.datetime
+            && lhs.latitude == rhs.latitude
+            && lhs.longitude == rhs.longitude
+            && lhs.passes == rhs.passes
+    }
+    
     let altitude: Int
     let datetime: Int
     let latitude: Float
     let longitude: Float
     let passes: Int
+    
+    init(altitude: Int, datetime: Int, latitude: Float, longitude: Float, passes: Int) {
+        self.altitude = altitude
+        self.datetime = datetime
+        self.latitude = latitude
+        self.longitude = longitude
+        self.passes = passes
+    }
 }
---
+++ ISSTests/Data Types/PassOverRequestTests.swift
@@ -0,0 +1,66 @@
+//
+//  PassOverRequestTests.swift
+//  ISSTests
+//
+//  Created by Alex Ogorek on 1/25/18.
+//  Copyright © 2018 kata. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import ISS
+
+class PassOverRequestTests: QuickSpec {
+    override func spec() {
+        describe("PassOverRequest") {
+            describe("equality") {
+                it("returns true when all are equal") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    
+                    expect(item1).to(equal(item2))
+                }
+                
+                it("returns false when altitude is different") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 20, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+                
+                it("returns false when datetime is different") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 10, datetime: 20, latitude: 10.5, longitude: 20.5, passes: 10)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+                
+                it("returns false when latitude is different") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 10, datetime: 10, latitude: 20.5, longitude: 20.5, passes: 10)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+                
+                it("returns false when longitude is different") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 30.5, passes: 10)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+                
+                it("returns false when passes is different") {
+                    let item1 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 10)
+                    let item2 = PassOverRequest(altitude: 10, datetime: 10, latitude: 10.5, longitude: 20.5, passes: 20)
+                    
+                    expect(item1).notTo(equal(item2))
+                }
+            }
+            
+            describe("init from dictionary") {
+                
+            }
+        }
+    }
+}
